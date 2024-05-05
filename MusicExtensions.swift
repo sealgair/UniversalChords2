639beed3fd553c435b210644754d4ab8bd490070
Chase Caster <sealgair@gmail.com>
2024-05-04 22:30:55 -0500

    basic chord picking

---
+++ MusicExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  MusicExtensions.swift
+//  UniversalChords
+//
+//  Created by Chase Caster on 5/4/24.
+//
+
+import Foundation
+import MusicTheory
+
+extension ChordThirdType: Identifiable {
+    public var id: Self { self }
+}
+
+extension ChordFifthType: Identifiable {
+    public var id: Self { self }
+}
+
+extension Key: Identifiable {
+    public var id: Self { self }
+}
--- UniversalChords/ContentView.swift
+++ UniversalChords/ContentView.swift
@@ -6,14 +6,53 @@
 //
 
 import SwiftUI
+import MusicTheory
 
 struct ContentView: View {
+    @State private var note = Key("c")
+    @State private var thirdType = ChordThirdType.major
+    @State private var fifthType = ChordFifthType.perfect
+    @State private var sixthType = false
+    private var chordType: ChordType {
+        ChordType(
+            third: thirdType,
+            fifth: fifthType,
+            sixth: sixthType ? ChordSixthType() : nil
+        )
+    }
+    private var chord: Chord {
+        return Chord(type: chordType, key: note)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(chord.description)
+            Picker("Chord Third Type", selection: $thirdType) {
+                ForEach(ChordThirdType.all) { chordType in
+                    Text(chordType.description).tag(chordType)
+                }
+            }.pickerStyle(.segmented)
+            Picker("Chord Fifth Type", selection: $fifthType) {
+                ForEach(ChordFifthType.all) { chordType in
+                    Text(chordType.description).tag(chordType)
+                }
+            }.pickerStyle(.segmented)
+            HStack {
+                HStack {
+                    ForEach(chord.keys) { key in
+                        Text(key.description)
+                    }
+                }.frame(maxWidth: .infinity)
+                Picker("Note", selection: $note) {
+                    ForEach(0...3, id: \.self) { i in
+                        ForEach(Key.keysWithSharps) { note in
+                            Text(note.description).tag(note)
+                        }
+                    }
+                }
+                .pickerStyle(.wheel)
+                .frame(maxWidth: 80, maxHeight: .infinity)
+            }.frame(maxHeight: .infinity)
         }
         .padding()
     }
