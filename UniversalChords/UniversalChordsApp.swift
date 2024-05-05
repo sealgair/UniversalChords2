e56c9d5f81d5d33b63408c126e90b88afc31fd12
Chase Caster <sealgair@gmail.com>
2024-05-04 20:10:11 -0500

    Initial Commit

---
+++ UniversalChords/UniversalChordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalChordsApp.swift
+//  UniversalChords
+//
+//  Created by Chase Caster on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversalChordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
